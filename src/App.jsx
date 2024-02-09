b36c91dd0f0d940e522851a1fb8bd99adba90dc7
Volodymyr <babihuk888@gmail.com>
2024-02-09 19:12:16 +0200

    start study useRef

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+
+  return (
+    // У більшості випадків useRef використовується щоб отримати посилання на ДОМ елемент
+    <h1 className='text-5xl font-bold text-blue-400 underline'>useRef - Xук посилання на DOM елемент</h1>
+  )
+}
+
+export default App
